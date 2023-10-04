57ffd5949554b462513d2c922018afcfea9dce33
Chonlasak Tumna <137064083+ChonlasakThamana@users.noreply.github.com>
2023-10-04 11:05:57 +0700

    12-1

--- Homework/HW12/hw12-1-basic-structure.cpp
+++ Homework/HW12/hw12-1-basic-structure.cpp
@@ -39,4 +39,51 @@
         Age : 21 years old
         Score : 87.60 points
 
-*/
+*/#include <stdio.h>
+
+struct Person {
+    char Name[20];
+    int Age;
+    char Sex;
+    float Score;
+};
+
+void printPersonInfo(struct Person person) {
+    printf("--| Person Information |--\n");
+    printf("Name : %s (%c)\n", person.Name, person.Sex);
+    printf("Age : %d years old\n", person.Age);
+    printf("Score : %.2f points\n", person.Score);
+}
+
+int main() {
+    struct Person A, B;
+
+    // Input data for Person A
+    printf("Person A\n");
+    printf("Name : ");
+    scanf("%s", A.Name);
+    printf("Age : ");
+    scanf("%d", &A.Age);
+    printf("Sex (M/F) : ");
+    scanf(" %c", &A.Sex);
+    printf("Score : ");
+    scanf("%f", &A.Score);
+
+    // Input data for Person B
+    printf("\nPerson B\n");
+    printf("Name : ");
+    scanf("%s", B.Name);
+    printf("Age : ");
+    scanf("%d", &B.Age);
+    printf("Sex (M/F) : ");
+    scanf(" %c", &B.Sex);
+    printf("Score : ");
+    scanf("%f", &B.Score);
+
+    // Display information for both persons
+    printPersonInfo(A);
+    printf("\n");
+    printPersonInfo(B);
+
+    return 0;
+}
